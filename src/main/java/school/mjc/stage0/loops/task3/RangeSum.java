c64f056a76a7e6e540738c652ff97ad68414abf1
Nadejda <nadyash295923@gmail.com>
2024-04-11 21:22:25 +0300

    done RS

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -1,6 +1,13 @@
 package school.mjc.stage0.loops.task3;
+/*1. Range sum.
+Write a program that will print the sum between numbers given as method arguments:
 
+
+*/
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+
+        for (int i = firstBoarder, sum = 0; i <=secondBoarder; i++)
+            sum +=i;
     }
 }
