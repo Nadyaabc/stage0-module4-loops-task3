8883e4f7ddf8d3c7eef513fcafb160a3f09af277
Nadejda <nadyash295923@gmail.com>
2024-04-11 22:04:10 +0300

    upd

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
-        int a = (first<second ? first:second);
+        int a = (first<=second ? first:second);
         int ans = 1;
         for (int i = 1; i <= a;i++ )
         {
