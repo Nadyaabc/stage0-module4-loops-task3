a63cbc88639d0706d9819bc96ed7f7450e13649a
Nadejda <nadyash295923@gmail.com>
2024-04-11 22:02:53 +0300

    done NS

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -4,7 +4,7 @@ public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
         System.out.println(0);
         System.out.println(1);
-        for (int i =0,k = 1; k < lastFibonacci; k++, i++)
+        for (int i =0,k = 1; k < lastFibonacci-1; k++, i++)
         {
             System.out.println(i+k);
         }
